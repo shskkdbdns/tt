abe6a80dac6f866d321fc4ed35e36ab9f0dab178
shskkdbdns <yasirmaroof159@gmail.com>
2025-03-07 01:25:22 +0530

    Update s4.py

--- s4.py
+++ s4.py
@@ -12,7 +12,7 @@ import os
 # @NOOB_AM GRP# insert your Telegram bot token here
 
     
-bot = telebot.TeleBot('7921815407:AAGfc_MBjUjS7t3vrHo7d-dkpG1WcLyHyEs')
+bot = telebot.TeleBot('7921815407:AAGCISJGb_4lD2lUv1n4uXZSc46PQQ8BtKA')
 
 # @NOOB_AM GRP # Admin user IDs
 admin_id = ["1257888659"]
